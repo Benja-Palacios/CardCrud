ec5736a8558bd310512fd060773773c7cd5e2125
Benjamin <benjaminmpalacios@gmail.com>
2023-11-12 16:51:11 -0600

    Crear proyecto crud

---
+++ src/main/java/com/crud/card/CardCrud/CardCrudApplication.java
@@ -0,0 +1,13 @@
+package com.crud.card.CardCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardCrudApplication.class, args);
+	}
+
+}
